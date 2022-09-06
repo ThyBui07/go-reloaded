af91652e609d0a05c7acab8621d5b3ac2f63ec0e
ThyBui07 <buithinguyenthy@gmail.com>
2022-09-06 08:14:50 +0300

    final

--- main.go
+++ main.go
@@ -14,16 +14,26 @@ func main() {
 	stringI := readFile(input)
 	//regex for fixing whitespace inside (up/low/cap, 2)
 
-	regPunc := `\s*(\.|\,|\!|\?\:|\;)([[:punct:]])*\s*`
-	stringI = regexp.MustCompile(regPunc).ReplaceAllStringFunc(stringI, addSpace)
-	fmt.Println(stringI)
+	regQuote := `\s\'\s*(.+?)\s*\'`
+	stringI = regexp.MustCompile(regQuote).ReplaceAllString(stringI, " '$1'")
+	//fmt.Println(stringI)
 
-	regQuote := `\'\s*(.+?)\s*\'`
-	stringI = regexp.MustCompile(regQuote).ReplaceAllString(stringI, "'$1'")
+	//regPunc := `\s*(\.|\,|\!|\?\:|\;)([[:punct:]])*\s*`
+	//stringI = regexp.MustCompile(regPunc).ReplaceAllStringFunc(stringI, addSpace)
+	//fmt.Println(stringI)
+
+	regVowelLower := `(a)\s([aeyiuoh]+)`
+	stringI = regexp.MustCompile(regVowelLower).ReplaceAllString(stringI, "an $2")
+	//fmt.Println(stringI)
+
+	regVowelUpper := `(A)\s([aeyiuoh]+)`
+	stringI = regexp.MustCompile(regVowelUpper).ReplaceAllString(stringI, "An $2")
+	//fmt.Println(stringI)
 
 	regIns := `\((up|low|cap)(,\s*(\d*))\)`
 	stringI = regexp.MustCompile(regIns).ReplaceAllStringFunc(stringI, replaceWhiteSpace)
 	//split the string into array
+
 	arrayInput := strings.Split(stringI, " ")
 	var arrayOutput []string
 	for i, e := range arrayInput {
@@ -76,14 +86,14 @@ func main() {
 				}
 			case instance == "cap":
 				if numOfInstance == 0 {
-					arrayInput[i-1] = strings.ToTitle(arrayInput[i-1])
+					arrayInput[i-1] = strings.Title(arrayInput[i-1])
 				} else {
 					if numOfInstance > len(arrayInput[:i]) {
 						fmt.Print("too big number")
 						os.Exit(0)
 					} else {
 						for x := numOfInstance; x >= 1; x-- {
-							arrayInput[i-x] = strings.ToTitle(arrayInput[i-x])
+							arrayInput[i-x] = strings.Title(arrayInput[i-x])
 						}
 					}
 				}
@@ -93,7 +103,18 @@ func main() {
 	}
 
 	fmt.Println("array output =", arrayOutput)
-	fmt.Println("array input =", arrayInput)
+
+	strOutput := strings.Join(arrayInput, " ")
+
+	fmt.Println(strOutput)
+
+	regBracket := `\(.+?\)`
+	strOutput = regexp.MustCompile(regBracket).ReplaceAllString(strOutput, "")
+
+	regPunc := `\s*(\.|\,|\!|\?\:|\;)([[:punct:]])*\s*`
+	strOutput = regexp.MustCompile(regPunc).ReplaceAllStringFunc(strOutput, addSpace)
+
+	fmt.Println(strOutput)
 }
 
 // matched, err := regexp.MatchString(`foo.*`, "seafood")
